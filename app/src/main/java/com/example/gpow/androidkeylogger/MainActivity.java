1d51cb750fe27c2369d4152f1a6b1e1374d45738
gokulrajanpillai <gokulrajanpillai@gmail.com>
2022-07-28 23:59:13 +0200

    Updated app icon

---
+++ app/src/main/java/com/example/gpow/androidkeylogger/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.gpow.androidkeylogger;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        updateText();
+    }
+
+    private void updateText() {
+        TextView textView = findViewById(R.id.logs);
+        textView.setText("Logs: " + loadContents());
+    }
+
+    public static Context getContext() {
+        return getContext();
+    }
+
+    private String loadContents() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String logs = preferences.getString("KeyLogger", "No logs found");
+        return logs;
+    }
+}
